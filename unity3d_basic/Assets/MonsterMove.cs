dcbc177a0a32f837d46bd637e527349562b98eb7
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2025-08-26 17:49:31 +0900

    (2025-08-26) 유니티 코루틴 실습 및 예제2

---
+++ unity3d_basic/Assets/MonsterMove.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMove : MonoBehaviour
+{
+    // 2d 월드에서 랜덤한 위치로 이동하는 코드를 작성해줘
+    // 이동 속도는 얼마인가,
+    // 이동 하는 방식은 무엇인가? rigidbody2d를 이용한 물리엔진 방식입니다.
+    // 서로 충돌했을 때는 어떤 일인가?
+
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private Rigidbody2D rigid;
+    private Vector2 targetVector;
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();    
+        // 중앙으로 이동하세요.
+        targetVector = SetPositionToCenter();
+
+        rigid.velocity = targetVector.normalized * moveSpeed;
+    }
+
+    private Vector2 SetPositionToCenter()
+    {
+        return Vector2.zero - (Vector2)transform.position;
+    }
+}
