6dd050185fc520b3d650bd0b56a86bec7e50668d
GameLecture2024 <158019948+GameLecture2024@users.noreply.github.com>
2025-08-29 17:49:16 +0900

    (2025-08-29) 거리를 이용한 AI 로직 구현

--- unity3d_basic/Assets/Scripts/NPC/NPC.cs
+++ unity3d_basic/Assets/Scripts/NPC/NPC.cs
@@ -13,7 +13,11 @@ public class NPC : MonoBehaviour
     Rigidbody2D rigidbody2D;
     BoxCollider2D boxCollider2D;
 
-    private Vector2 currentTargetPos; // 언제 Stop을 해야 하는가?
+
+    [SerializeField] private Vector2 currentTargetPos; // 언제 Stop을 해야 하는가?
+    [SerializeField] private bool isMoving; // 목적지 도착 후에 한번만 위치를 재설정하기 위함
+    Transform playerPos;
+    // 정찰, 추적
 
     private void Awake()
     {
@@ -31,13 +35,37 @@ public class NPC : MonoBehaviour
 
     private void Start()
     {
-        Patrol();
+        SetRandomPosition();
     }
 
     private void Update()
     {
-        // 목적지까지 이동한 후 멈춰라
-        Stop();
+        // 언제 정찰?  현재 플레이어와의 거리에 따라서 정찰을 할지 추적을 할지 정한다.
+        // test1 대신에 Vector2.Distance 함수를 사용해서 코드를 활용해보세요.
+
+        if (IsPatrol())
+            Patrol();
+        else
+            Chase();
+
+        // Instantiate
+        // Enemy attack. 공격하기 위한 최소 거리
+
+        // 언제 추적?
+    }
+
+    // 현재 상태를 체크해주는 함수
+    bool IsPatrol()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        if (Vector2.Distance(transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < npcInfo.patrolDistance)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
     }
 
     public void Patrol()
@@ -46,33 +74,74 @@ public class NPC : MonoBehaviour
         MoveTargetPoint();  
     }
 
-    public void Stop()
+    public void Chase()
     {
-        // Vector 클래스 안에는 Distance함수가 존재한다.
-
-        //if( ) // 목적지에 도착했으면 멈춰라
-        //{
-        //    rigidbody2D.velocity = Vector2.zero;
-        //}      
+        // Player를 어떻게 받아올 것인가. 게임오브젝트의 이름이 player, tag가 player오브젝트를 전달해준다.      
+        SetPoistion(playerPos.position);
+        MoveTargetPoint();
     }
 
-    public void WaitTime(float time)
-    {
-
-    }
 
     private void MoveTargetPoint()
     {
         // 속도의 랜덤값 구현
         float moveSpeed = Random.Range((float)npcInfo.MinSpeed, npcInfo.MaxSpeed);
-
-        // 위치의 랜덤값 표현
-        Vector2 randomPosition = (Vector2)transform.position + Random.insideUnitCircle * npcInfo.PatrolRadius;
-
-        currentTargetPos = randomPosition;
         // 이동 속도, 이동해야할 위치 현재 위치 (이동해야할 방향)
-        // 방향 * 속도 = 이동
         // 두 백터 위치 값과 속도를 사용해서 => 코드를 구현하세요.  이동해야해야할 방향* 속도;
-        rigidbody2D.velocity = (randomPosition - (Vector2)transform.position).normalized * moveSpeed;
+
+        // 목적지까지 도착했으면 멈춰라
+        // Vector Distance함수
+        if(Vector2.Distance(transform.position, currentTargetPos) < npcInfo.stopDistance) // 대상과의 멈추기 위한 거리 StopDistance
+        {
+            rigidbody2D.velocity = Vector2.zero;
+            isMoving = true;
+            // 잠시 기다리는 시간 부여. 
+            // 이벤트 처리. StopEvent
+            //if(isMoving)
+            //    StartCoroutine(SetRandomPositionCoroutine());
+
+            if(IsPatrol())
+                SetRandomPosition();
+        }
+        else  // 그렇지 않으면 이동하라
+        {
+            rigidbody2D.velocity = (currentTargetPos - (Vector2)transform.position).normalized * moveSpeed;
+        }      
+    }
+
+    private void SetRandomPosition()
+    {
+        // 위치의 랜덤값 표현
+        currentTargetPos = (Vector2)transform.position + Random.insideUnitCircle * npcInfo.PatrolRadius;
+    }
+
+    public void SetPoistion(Vector2 position)
+    {
+        currentTargetPos = position;
+    }
+
+    private IEnumerator SetRandomPositionCoroutine()
+    {
+        isMoving = false;
+        yield return new WaitForSeconds(1f);
+        SetRandomPosition();
+    }
+
+    private void OnDrawGizmos()
+    {
+        //DrawChaseCircle();
+    }
+
+    // 기즈모를 그리는 특수한 함수
+    private void OnDrawGizmosSelected()
+    {
+        DrawChaseCircle();
+    }
+
+    private void DrawChaseCircle()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, npcInfo.patrolDistance);
     }
 }
